b82c5a4452c852a113ea891f1a3cfe7a9d2190f2
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2021-06-16 09:09:55 -0700

    added button with listener. On click the toast shows up.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // Activity is a subtype of context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll!!"
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            
+        }
+
     }
 }
