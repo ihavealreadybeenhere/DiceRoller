bd187814dbb5fc0b9a06539318b1ca2e594b0386
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2021-06-16 11:08:58 -0700

    Make app images compatible to the older versions af the android

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
